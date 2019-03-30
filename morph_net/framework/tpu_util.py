10517901b28eb141b58fd1774f35fe7106888f9f
pkch <pkch@google.com>
2019-03-30 15:36:10 -0700

    Enable cifar misc summaries.
    PiperOrigin-RevId: 241158874

--- morph_net/framework/tpu_util.py
+++ morph_net/framework/tpu_util.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from absl import logging
 import tensorflow as tf
 
 
@@ -11,10 +12,12 @@ def maybe_convert_to_cpu(gamma):
   """Convert TPU gammas to the equivalent variables available on CPU."""
   if gamma.op.type != 'ReadVariableOp' or 'BatchNorm' not in gamma.op.name:
     # We are looking for resource variables containing gammas. This isn't one.
+    logging.info('Not replacing %s.', gamma.name)
     return gamma
 
   with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
     base_name = gamma.name.rsplit('/', 1)[0]
     shape = gamma.shape
     cpu_variable = tf.get_variable(base_name + '/gamma', shape=shape)
+    logging.info('Replacing %s with %s.', gamma.name, cpu_variable.name)
     return tf.convert_to_tensor(cpu_variable)
