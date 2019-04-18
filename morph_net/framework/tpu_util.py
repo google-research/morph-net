cdf0d56682f627a4240ac8c959e62108c3434602
pkch <pkch@google.com>
2019-04-18 00:50:11 -0700

    Internal changes only
    PiperOrigin-RevId: 244141627

--- morph_net/framework/tpu_util.py
+++ morph_net/framework/tpu_util.py
@@ -17,11 +17,46 @@ def get_variable_name(read_variable_op):
 
 
 def maybe_convert_to_variable(tensor):
-  """Convert TPU tensor to the ResourceVariable if possible."""
+  """Convert TPU variable to be usable outside a while loop.
+
+  Args:
+    tensor: A tf.Tensor.
+
+  Returns:
+    If tensor is the output of reading a ResourceVariable, replace it with an
+    equivalent tensor produced outside the while loop. Otherwise, return the
+    original tensor.
+  """
   op = tensor.op
   if op.type != 'ReadVariableOp':
-    # Cannot convert.
+    # No need to convert.
     return tensor
-  with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
-    shape = tensor.shape
-    return tf.get_variable(get_variable_name(op), shape=shape)
+  with tf.variable_scope(
+      # We don't want to add any new scope, just reuses the outside scope.
+      # The only reason we are using variable_scope is because there's no other
+      # way to request reuse.
+      name_or_scope=tf.get_variable_scope(),
+      # We are looking for a reference to an existing variable, so we want to
+      # raise an exception if variable is not found.
+      reuse=True,
+  ):
+    # The name already contains (as slashes) all the scope we need.
+    # However, we cannot reset scope to root because TF has no such API;
+    # see https://yaqs.googleplex.com/eng/q/5549333873426432, and
+    # https://github.com/tensorflow/tensorflow/issues/7731#issuecomment-329694973
+    # Therefore, we have to hack the variable name.
+
+    variable_name = get_variable_name(op)
+    current_scope = tf.get_variable_scope().name
+    tf.logging.info('Converting. variable_name %s current_scope %s tensor %s',
+                    variable_name, current_scope, tensor)
+    if current_scope:
+      current_scope += '/'
+      # Strip current_scope from the variable name.
+      assert variable_name.startswith(current_scope)
+      relative_name = variable_name[len(current_scope):]
+    else:
+      # current_scope == '' if subplan variable scope is not set, or if it's set
+      # but w are creating the regularizer for summaries rather than losses.
+      relative_name = variable_name
+    return tf.get_variable(relative_name)
