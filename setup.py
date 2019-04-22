eabe3848695e79d87bf4a2e542e4214894815909
Max Moroz <pkch@google.com>
2019-04-22 15:35:18 -0700

    Fix LICENSE
    PiperOrigin-RevId: 244748670

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
 )
