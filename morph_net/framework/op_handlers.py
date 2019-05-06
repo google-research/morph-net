a01a02aea8269ee5e57dd35772ebb6b3daff0c42
Elad Eban <elade@google.com>
2019-05-06 15:41:37 -0700

    Unify op_handler_dict creation.
    Presents symmetric treatment for GroupLasso and Gamma based network regularizers.
    
    PiperOrigin-RevId: 246911353

--- morph_net/framework/op_handlers.py
+++ morph_net/framework/op_handlers.py
@@ -14,55 +14,44 @@ from morph_net.framework import leaf_op_handler
 from morph_net.framework import output_non_passthrough_op_handler
 
 
-def get_gamma_op_handler_dict():
+def _get_base_op_hander_dicts():
   return collections.defaultdict(
       grouping_op_handler.GroupingOpHandler, {
-          'Conv2D':
-              output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
           'ConcatV2':
               concat_op_handler.ConcatOpHandler(),
           'DepthToSpace':
               output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
           'DepthwiseConv2dNative':
               depthwise_convolution_op_handler.DepthwiseConvolutionOpHandler(),
-          'MatMul':
+          'ExpandDims':
               output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
-          'TensorArrayGatherV3':
-              leaf_op_handler.LeafOpHandler(),
           'RandomUniform':
               leaf_op_handler.LeafOpHandler(),
           'Reshape':
+              leaf_op_handler.LeafOpHandler(),
+          'Shape':
               leaf_op_handler.LeafOpHandler(),
           'SpaceToDepth':
               leaf_op_handler.LeafOpHandler(),
-          'Shape':
-              leaf_op_handler.LeafOpHandler(),
-          'Transpose':
-              output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
-          'ExpandDims':
-              output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
-      })
-
-
-def get_group_lasso_op_handler_dict():
-  return collections.defaultdict(
-      grouping_op_handler.GroupingOpHandler, {
-          'ConcatV2':
-              concat_op_handler.ConcatOpHandler(),
-          'DepthToSpace':
-              output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
-          'DepthwiseConv2dNative':
-              depthwise_convolution_op_handler.DepthwiseConvolutionOpHandler(),
-          'RandomUniform':
-              leaf_op_handler.LeafOpHandler(),
-          'Reshape':
-              leaf_op_handler.LeafOpHandler(),
-          'Shape':
+          'StridedSlice':
               leaf_op_handler.LeafOpHandler(),
           'TensorArrayGatherV3':
               leaf_op_handler.LeafOpHandler(),
           'Transpose':
               output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
-          'StridedSlice':
-              leaf_op_handler.LeafOpHandler(),
       })
+
+
+def get_gamma_op_handler_dict():
+  op_handler_dict = _get_base_op_hander_dicts()
+  op_handler_dict.update({
+      'Conv2D':
+          output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
+      'MatMul':
+          output_non_passthrough_op_handler.OutputNonPassthroughOpHandler(),
+  })
+  return op_handler_dict
+
+
+def get_group_lasso_op_handler_dict():
+  return _get_base_op_hander_dicts()
---
+++ morph_net/framework/op_handlers_test.py
@@ -0,0 +1,26 @@
+"""Tests for framework.op_handlers."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from morph_net.framework import op_handlers
+
+import tensorflow as tf
+
+
+class OpHandlersTest(tf.test.TestCase):
+
+  def test_dict_logic(self):
+    gamma_dict = op_handlers.get_gamma_op_handler_dict()
+    self.assertIn('Conv2D', gamma_dict)
+    self.assertIn('MatMul', gamma_dict)
+    group_lasso_dict = op_handlers.get_group_lasso_op_handler_dict()
+    self.assertNotIn('Conv2D', group_lasso_dict)
+    self.assertNotIn('MatMul', group_lasso_dict)
+    for op in group_lasso_dict:
+      self.assertIn(op, gamma_dict)
+
+
+if __name__ == '__main__':
+  tf.test.main()
