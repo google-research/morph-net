6a1093554222259cfedf6b1201ce434324708744
Max Moroz <pkch@google.com>
2019-06-19 15:48:19 -0700

    Store tensors from TPU into variables, for use on CPU.
    New features:
    * tpu_util.write_to_variable will make the tensor available on CPU.
    * tpu_util.read_from_variable will retrieve the previously stored tensor.
    
    PiperOrigin-RevId: 254087152

--- morph_net/framework/tpu_util.py
+++ morph_net/framework/tpu_util.py
@@ -4,7 +4,32 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import contextlib2
 import tensorflow as tf
+
+_run_on_cpu = False
+
+
+@contextlib2.contextmanager
+def run_on_cpu():
+  """Provide a context for the code that needs to run on CPU.
+
+  Not thread-safe.
+
+  Yields:
+    None.
+  """
+  global _run_on_cpu
+  original_run_on_cpu = _run_on_cpu
+  _run_on_cpu = True
+  try:
+    yield
+  finally:
+    _run_on_cpu = original_run_on_cpu
+
+
+def is_on_cpu():
+  return _run_on_cpu
 
 
 def get_variable_name(read_variable_op):
@@ -42,3 +67,32 @@ def maybe_convert_to_variable(tensor):
     tf.logging.info('Converting tensor %s --> tf.get_variable(%s)',
                     tensor, variable_name)
     return tf.get_variable(variable_name)
+
+
+var_store = {}
+
+
+def write_to_variable(tensor):
+  """Saves a tensor for later retrieval on CPU."""
+  # Only relevant for debugging.
+  debug_name = 'tpu_util__' + tensor.name.split(':')[0].split('/')[-1]
+  with tf.variable_scope('', reuse=False):
+    variable = tf.get_variable(
+        name=debug_name,
+        shape=tensor.shape,
+        dtype=tensor.dtype,
+        trainable=False,
+        use_resource=True)
+  var_store[tensor] = variable
+  with tf.control_dependencies([variable.assign(tensor)]):
+    return tf.identity(tensor)
+
+
+def read_from_variable(tensor):
+  """Retrieves (a possibly stale copy of) the previously stored tensor."""
+  if is_on_cpu():
+    # Stale read, but on CPU that's all we can do without adding to loop vars.
+    return var_store[tensor]
+  else:
+    # Current read, but only works on TPU.
+    return tensor
