f22ab125f9add15aa5f126fb18e92799d63919e0
The MorphNet Team <mn_robot@google.com>
2020-02-21 13:35:36 -0800

    Explicitly replace "import tensorflow" with "tensorflow.compat.v1" for TF2.x migration
    PiperOrigin-RevId: 296501193

--- examples/slim/datasets/build_visualwakewords_data.py
+++ examples/slim/datasets/build_visualwakewords_data.py
@@ -77,7 +77,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from datasets import build_visualwakewords_data_lib
 
 flags = tf.app.flags
--- examples/slim/datasets/flowers.py
+++ examples/slim/datasets/flowers.py
@@ -23,7 +23,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 from datasets import dataset_utils
 
